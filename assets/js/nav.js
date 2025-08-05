083ead28e27c83e03d912057cc3b7c01764a684a
Esra-K <esra.ka170@gmail.com>
2025-08-05 13:54:41 +0330

    reformat file

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.getElementById("dynamic-nav");
     const sections = document.querySelectorAll(".main-content section[id]");
-  
+
     sections.forEach(section => {
-      const id = section.id;
-      const title = section.querySelector("h2")?.innerText || id;
-      const link = document.createElement("a");
-      link.href = `#${id}`;
-      link.textContent = title;
-      nav.appendChild(link);
+        const id = section.id;
+        const title = section.querySelector("h2")?.innerText || id;
+        const link = document.createElement("a");
+        link.href = `#${id}`;
+        link.textContent = title;
+        nav.appendChild(link);
     });
-  });
-  
+});
