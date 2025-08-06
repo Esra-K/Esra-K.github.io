95eab12ac7fede375cbf8a5fe5642e38a4fe9768
Esra-K <esra.ka170@gmail.com>
2025-08-07 02:14:20 +0330

    global hamburger button

--- assets/js/sidebar-toggle.js
+++ assets/js/sidebar-toggle.js
@@ -1,8 +1,13 @@
-// Toggle the sidebar in/out on mobile
+// for hamburger
 document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.getElementById("sidebar-toggle");
   const sidebar = document.getElementById("sidebar");
+
+  // start “collapsed” on page load if you want:
+  // sidebar.classList.add('collapsed');
+
   toggle.addEventListener("click", () => {
     sidebar.classList.toggle("active");
+    sidebar.classList.toggle("collapsed");
   });
 });
