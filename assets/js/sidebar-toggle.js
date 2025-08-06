0da3681f6c719272c55d3cf7ff3271ed542c8bf6
Esra-K <esra.ka170@gmail.com>
2025-08-07 00:32:38 +0330

    collapsable sideba w hamburger button

---
+++ assets/js/sidebar-toggle.js
@@ -0,0 +1,5 @@
+/* for hamburger */
+// Toggle sidebar visibility on small screens
+document.getElementById("sidebar-toggle").addEventListener("click", () => {
+  document.getElementById("sidebar").classList.toggle("active");
+});
