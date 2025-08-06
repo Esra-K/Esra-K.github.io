2d8bfa8562f91dde501b62b4114ce2632929a146
Esra-K <esra.ka170@gmail.com>
2025-08-07 02:29:10 +0330

    global hamburger button - fixing

--- assets/js/sidebar-toggle.js
+++ assets/js/sidebar-toggle.js
@@ -3,11 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.getElementById("sidebar-toggle");
   const sidebar = document.getElementById("sidebar");
 
-  // start “collapsed” on page load if you want:
-  // sidebar.classList.add('collapsed');
-
   toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
+    console.log("Hamburger clicked");
     sidebar.classList.toggle("collapsed");
+    sidebar.classList.toggle("active");
   });
 });
